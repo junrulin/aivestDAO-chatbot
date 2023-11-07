8a10b78b1ae6cce268ce86e3292fb78701cd7e7d
junrulin <34433306+junrulin@users.noreply.github.com>
2023-11-07 14:43:18 +0800

    feat: update greeting

--- src/modules/layout.py
+++ src/modules/layout.py
@@ -33,7 +33,7 @@ class Layout:
         with st.form(key="my_form", clear_on_submit=True):
             user_input = st.text_area(
                 "Query:",
-                placeholder="Ask me anything about the document...",
+                placeholder="I'm the co-owner at Bankless, optimistic storyteller of frontier technology. Let me share my insights!",
                 key="input",
                 label_visibility="collapsed",
             )
